fee8d6ad080535771c7bb50b33e649b64aabf46d
tvan <toni.van@zitrogames.com>
2021-11-13 17:58:34 +0100

    Añade primer scrapping

--- datascrapping.py
+++ datascrapping.py
@@ -1,2 +1,32 @@
 import pandas as pd
 import numpy as np
+from loadFiles import *
+import time
+
+# Loading data from housing prices
+codes = ["Est_Mercat_Immobiliari_Lloguer_Mitja_Mensual", "Est_Mercat_Immobiliari_Lloguer_Nombre_Contractes"]
+data = []
+contador_data = 0
+#for k in codes:
+   # data[contador_data] = loadFiles(k)
+    #contador_data += 1
+data = loadFiles(codes[0])
+print(data)
+columnas = data.columns
+keys = ["Any", "Trimestre","Codi_Districte", "Nom_Districte", "Codi_Barri", "Nom_Barri"]
+boolean = False
+contador = 0
+for j in keys:
+    if j in columnas:
+        contador += 1 
+if contador == len(keys):
+    for j in columnas:
+        if j not in keys:
+            if data[j].dtype == "int64" or data[j].dtype == "float64":
+                print(j,"HOLAAAAA")
+                df_master = data.groupby(['Any', 'Trimestre','Codi_Districte', 'Nom_Districte', 'Codi_Barri','Nom_Barri']).sum()
+df_master = df_master.reset_index()
+#for j in codes:
+#    data = loadFiles(j)
+#    df.concat(data)
+print(df_master)
