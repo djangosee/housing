644f350914c92b6a8d5495ba0a72995220898b0c
anrodriguez <antonio.rodriguez@globalzitro.com>
2021-11-14 13:16:53 +0100

    Mejora housing.py

--- housing.py
+++ housing.py
@@ -20,15 +20,17 @@ def main():
 
     parser.add_argument('code', nargs="?",type=str, help='OpenDataBcn code, example:Est_Mercat_Immobiliari_Lloguer_Mitja_Mensual')
     parser.add_argument('-m','--map', action="store_true", help="Open Bcn map example")
-    parser.add_argument('-d','--dataset',type=bool, action="store", help="Create csv file from code dataset")
+    parser.add_argument('-d','--dataset',action="store_true", help="Create csv file from code dataset")
 
     args = parser.parse_args()
 
     if args.map:
         print("Generating Map from OpenDataBcn...")
         openMapExample()
-#    if args.dataset:
-#        print("Saving dataset wth code:"+ args.code +"...")
-#        saveCodeAsCsv()
+    if args.dataset:
+        print("Saving dataset with code: "+ str(args.code))
+        df = loadFiles([args.code])
+        print("DataSet.csv generated!")
+        df.to_csv("DataSet.csv")
 
 main()
